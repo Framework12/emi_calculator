8ac5eca96c146f8987a9af2122165bdf1695502f
framework12 <rootcyber032@gmail.com>
2025-05-07 01:21:01 +0530

    first commit

--- src/config/api.js
+++
@@ -1,6 +0,0 @@
-export const API_CONFIG = {
-  baseUrl: process.env.EXCHANGE_RATE_BASE_URL,
-  apiKey: process.env.EXCHANGE_RATE_API_KEY,
-  getExchangeRateUrl: (currency = 'USD') => 
-    `${process.env.EXCHANGE_RATE_BASE_URL}/${process.env.EXCHANGE_RATE_API_KEY}/latest/${currency}`
-};
